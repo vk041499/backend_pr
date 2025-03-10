b1ce95be4392f1b27956b1903dbd8f4e9858c3a8
Vishal Kumar <125488544+vk041499@users.noreply.github.com>
2025-03-10 11:59:55 +0530

    add refreshAccessToken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -183,10 +183,57 @@ const logoutUser = asyncHandler(async (req,res) => {
     .json(new apiResponse(200, {},"User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler(async(req,res) =>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (incomingRefreshToken) {
+        throw new apiError(401, "Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+    
+        const user = await User.findById(decodedToken?._id)
+    
+        if (!user) {
+            throw new apiError(401, "Invalid Refresh_Token")
+        }
+    
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new apiError(401, "Refresh token is expired or used")
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken", newRefreshToken,options)
+        .json(
+            new apiResponse(
+                 200,
+                 {accessToken, refreshToken: newRefreshToken},
+                 "access token refresh successfully"
+            )
+        )
+    } catch (error) {
+        throw new apiError(401, error?.message || "Invalid refresh token")
+    }
+})
+
+
 
 export{
      registerUser,
      loginUser,
      logoutUser,
-    
+     refreshAccessToken,
 }
